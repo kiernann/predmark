3073949f6278e4e28e9bcb6ab8b6415a1e0a26b4
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-26 20:50:20 -0500

    Add partisan lean to results

--- data-raw/results18.R
+++ data-raw/results18.R
@@ -71,6 +71,8 @@ results18 <- forecast_results %>%
       .f = category,
       "Tilt D" = "Tossup (Tilt D)",
       "Tilt R" = "Tossup (Tilt R)"
+      "Like D" = "Likely D"
+      "Like R" = "Likely R"
     )
   ) %>%
   unite(
@@ -79,6 +81,99 @@ results18 <- forecast_results %>%
     sep = "-"
   ) %>%
   select(year = cycle, race, category, winner)
+
+
+# add partisan lean -------------------------------------------------------
+
+# Average difference between how a district votes and the country
+# Updated:  2018-11-19 at 16:13
+# Archived: 2018-04-04 at 16:05
+write_memento(
+  date = "2019-04-04",
+  dir = here("data-raw", "results"),
+  url = paste(
+    "https://raw.githubusercontent.com/fivethirtyeight/data/master",
+    "partisan-lean",
+    "fivethirtyeight_partisan_lean_DISTRICTS.csv",
+    sep = "/"
+  )
+)
+
+partisan_lean_DISTRICTS <- read_csv(
+  file = here("data-raw", "results", "fivethirtyeight_partisan_lean_DISTRICTS.csv"),
+  col_types = cols(
+    district = col_character(),
+    pvi_538 = col_character()
+  )
+)
+
+write_memento(
+  date = "2019-04-04",
+  dir = here("data-raw", "results"),
+  url = paste(
+    "https://raw.githubusercontent.com/fivethirtyeight/data/master",
+    "partisan-lean",
+    "fivethirtyeight_partisan_lean_STATES.csv",
+    sep = "/"
+  )
+)
+
+partisan_lean_STATES <- read_csv(
+  file = here("data-raw", "results", "fivethirtyeight_partisan_lean_STATES.csv"),
+  col_types = cols(
+    state = col_character(),
+    pvi_538 = col_character()
+  )
+)
+
+# Separate lean value and replace state name with state abbreviation
+lean_states <- partisan_lean_STATES %>%
+  separate(
+    col = pvi_538,
+    into = c("party", "lean"),
+    sep = "\\+",
+    convert = TRUE
+  ) %>%
+  rename(race = state) %>%
+  mutate(race = paste(state.abb, "S1", sep = "-"))
+
+# Seperate lean value and pad district number for race code
+lean_district <- partisan_lean_DISTRICTS %>%
+  separate(
+    col = pvi_538,
+    into = c("party", "lean"),
+    sep = "\\+",
+    convert = TRUE
+  ) %>%
+  separate(
+    col = district,
+    into = c("state", "race"),
+    sep = "\\-"
+  ) %>%
+  mutate(race = str_pad(race, width = 2, pad = "0")) %>%
+  unite(
+    col = race,
+    state, race,
+    sep = "-"
+  )
+
+# Turn single number into negative-positive spectrum
+lean18 <-
+  bind_rows(lean_states, lean_district) %>%
+  mutate(
+    lean = case_when(
+      party == "D" ~ lean * -1,
+      party == "R" ~ lean
+    )
+  )
+
+results18 <- left_join(
+  x = results18,
+  y = lean18,
+  by = "race"
+)
+
+results18 <- relocate(results18, lean, .before = category)
 
 # export results ----------------------------------------------------------
 
